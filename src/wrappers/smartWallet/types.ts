67540819987e1e4b6f2468c02451efefd103af91
Goki Rajesh <goki@goki.so>
2021-11-30 03:10:47 +0530

    Return the index of the proposed transaction

--- src/wrappers/smartWallet/types.ts
+++ src/wrappers/smartWallet/types.ts
@@ -1,5 +1,6 @@
 import type { TransactionEnvelope } from "@saberhq/solana-contrib";
-import type { PublicKey } from "@solana/web3.js";
+import type { PublicKey, TransactionInstruction } from "@solana/web3.js";
+import type BN from "bn.js";
 
 import type { SmartWalletData } from "../../programs";
 import type { SmartWalletWrapper } from ".";
@@ -17,6 +18,32 @@ export type PendingSmartWallet = {
 };
 
 export type PendingSmartWalletTransaction = {
+  /**
+   * Pubkey of the created transaction.
+   */
   readonly transactionKey: PublicKey;
+  /**
+   * Transaction to create the transaction.
+   */
   readonly tx: TransactionEnvelope;
+  /**
+   * Index of the transaction.
+   */
+  readonly index: number;
 };
+
+export interface NewTransactionArgs {
+  readonly proposer?: PublicKey;
+  /**
+   * Payer of the created transaction.
+   */
+  readonly payer?: PublicKey;
+  /**
+   * Instructions which compose the new transaction.
+   */
+  readonly instructions: TransactionInstruction[];
+  /**
+   * ETA of the new transaction.
+   */
+  readonly eta?: BN;
+}
