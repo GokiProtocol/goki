24ec3f8e3fad5ebe80d8c53bd430e6c85cda9321
michaelhly <michaelhly@gmail.com>
2022-03-09 00:37:09 -0600

    Fix cargo doc

--- programs/smart-wallet/src/state.rs
+++ programs/smart-wallet/src/state.rs
@@ -204,7 +204,7 @@ pub struct InstructionBuffer {
     ///
     /// This may be used to see if the owners on the multisig have changed
     pub owner_set_seqno: u32,
-    /// - If set to [crate::NO_ETA], the instructions in the [InstructionBuffer::buffer] may be executed at any time.
+    /// - If set to [crate::NO_ETA], the instructions in the [InstructionBuffer::instructions] may be executed at any time.
     /// - Otherwise, instructions may be executed at any point after the ETA has elapsed.
     pub eta: i64,
     /// Time denoting when buffer is ready to be executed.
