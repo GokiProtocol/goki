c7d47b8467a66505a0d53ac4b025e58811c5f59c
Goki Rajesh <goki@goki.so>
2022-04-18 15:57:19 +0530

    remove mochasetup.js

--- mochasetup.js
+++
@@ -1,5 +0,0 @@
-require("./.pnp.cjs").setup();
-require("ts-node/register");
-
-process.env.ANCHOR_PROVIDER_URL = "http://localhost:8899";
-process.env.ANCHOR_WALLET = `${require("os").homedir()}/.config/solana/id.json`;
