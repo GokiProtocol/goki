5b3c405b7db57b8e9ad902e427c9366ceb531e05
michaelhly <michaelhly@gmail.com>
2022-06-22 23:37:01 -0400

    Clean up types

--- src/programs/smartWallet.ts
+++ src/programs/smartWallet.ts
@@ -10,7 +10,6 @@ export type SmartWalletTypes = AnchorTypes<
   {
     smartWallet: SmartWalletData;
     transaction: SmartWalletTransactionData;
-    instructionBuffer: InstructionBufferData;
     subaccountInfo: SubaccountInfoData;
   },
   {
@@ -22,7 +21,6 @@ export type SmartWalletTypes = AnchorTypes<
 type Accounts = SmartWalletTypes["Accounts"];
 export type SmartWalletData = Accounts["SmartWallet"];
 export type SmartWalletTransactionData = Accounts["Transaction"];
-export type InstructionBufferData = Accounts["InstructionBuffer"];
 export type SubaccountInfoData = Accounts["SubaccountInfo"];
 
 export type SmartWalletInstruction = Omit<
